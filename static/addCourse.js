75c46a71ffb2c258c71e583175e24ca9cf9820f7
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2023-07-22 15:30:01 +0530

    bibiud

---
+++ static/addCourse.js
@@ -0,0 +1,9 @@
+console.log("hi")
+const courseClick = document.querySelector('.courseclick')
+
+
+function addingcourses(){
+    courseClick.addEventListener("click",()=>{
+        console.log("hi")
+    })
+}
