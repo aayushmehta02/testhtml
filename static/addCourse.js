7a55beeb7a88d28481d94e228573552ded9a23b7
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2023-07-24 14:31:57 +0530

    btnt

--- static/addCourse.js
+++ static/addCourse.js
@@ -1,9 +1,13 @@
 console.log("hi")
 const courseClick = document.querySelector('.courseclick')
+const right_coloumn = document.querySelector('.right_coloumn')
 
 
 function addingcourses(){
     courseClick.addEventListener("click",()=>{
         console.log("hi")
+        right_coloumn.style.filter = "blur"
+        
     })
 }
+addingcourses()
