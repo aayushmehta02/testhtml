3519c62ad0cb3c4296b685ead358df8d85c21007
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2023-07-25 21:55:28 +0530

    kfndk

--- static/addCourse.js
+++ static/addCourse.js
@@ -1,13 +1,44 @@
-console.log("hi")
+
 const courseClick = document.querySelector('.courseclick')
 const right_coloumn = document.querySelector('.right_coloumn')
+const addCourse_wrapper = document.querySelector('.addcourse_form_wrapper')
+const submit = document.querySelector('.submit')
+const course_name_input = document.querySelector('.course_name_input')
+const course_name_output = document.querySelector('.course_name_output')
+const year_input =document.querySelector('.year_input')
+const year_output = document.querySelector('.year_output')
+
+
+
+
+
+
+
+
 
 
 function addingcourses(){
     courseClick.addEventListener("click",()=>{
-        console.log("hi")
-        right_coloumn.style.filter = "blur"
+        addCourse_wrapper.classList.add("show")
+        submit.addEventListener("click",()=>{
+            addCourse_wrapper.classList.remove("show")
+            takeCourseInput()
+            takeYearInput()
+        })
         
     })
 }
 addingcourses()
+
+
+function takeCourseInput(){
+    course_name_output.innerText = course_name_input.value
+    console.log(course_name_input.value)
+    course_name_input.value=""
+}
+
+function takeYearInput(){
+    year_output.innerText = year_input.value
+    console.log(year_input.value)
+    year_input.value=""
+}
